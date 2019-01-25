f1bacc7cbfca452c1e41eb0257809305ba4a7c5e
Felix <f.samplexyt@gmail.com>
2019-01-25 12:36:49 +0100

    Updates

---
+++ precache-manifest.b9c457282a877c474b6143873addc6a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fda4de7fcdfae837c2a3",
+    "url": "/react-calculator/static/css/main.da4678a8.chunk.css"
+  },
+  {
+    "revision": "fda4de7fcdfae837c2a3",
+    "url": "/react-calculator/static/js/main.fda4de7f.chunk.js"
+  },
+  {
+    "revision": "8ab5e2a4f76d9557b6cd",
+    "url": "/react-calculator/static/js/1.8ab5e2a4.chunk.js"
+  },
+  {
+    "revision": "a2ecf2e68de309a6bd2b",
+    "url": "/react-calculator/static/js/runtime~main.a2ecf2e6.js"
+  },
+  {
+    "revision": "599986b1744284a5b775483e153e1613",
+    "url": "/react-calculator/index.html"
+  }
+];
